8e1a91d6ca7171c48f7a3d595f46f667cc62c85d
Nnorb87 <norbertnagy87@gmail.com>
2024-07-18 16:43:42 +0200

    Unit Move

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    private Vector3 targetPosition;
+
+    private void Update(){
+        float stoppingDistance = .1f;
+        if ( Vector3.Distance(transform.position, targetPosition) > stoppingDistance){
+            Vector3 moveDirection = (targetPosition - transform.position).normalized;
+            float moveSpeed = 4f;
+            transform.position += moveDirection * Time.deltaTime * moveSpeed;
+        }
+
+        if (Input.GetKeyDown(KeyCode.W)){
+            Move(new Vector3(4,0,4));
+        }
+
+    }
+
+    private void Move(Vector3 targetPosition){
+        this.targetPosition = targetPosition;
+
+    }
+
+}
